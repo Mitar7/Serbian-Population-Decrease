803b077bff19abf7f89606eb414355d4c740c20f
Mitar7 <mitar.milovanovic100@gmail.com>
2021-11-18 16:01:28 +0100

    Update 1.0

--- program.py
+++ program.py
@@ -1,5 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-df = pd.read_csv('proj.csv', index_col='Year')
-print(df['TotalPopulation'])
+msg = """
+    -----------------------------------
+    Serbia's population decline problem
+    -----------------------------------
+    1. Population by selected year
+"""
+
+# df = pd.read_csv('proj.csv')
+# print(df.loc['2030':, ['Year', 'TotalPopulation']])
+# print((df[df['TotalPopulation'] > 8000000]))
+
+
+def filter_by_year():
+    os.system('cls')
+    year = input('Please enter a year: ')
+    main()
+
+
+def main():
+    print(msg)
+    option = int(input("Please select option: "))
+    if option == 1:
+        filter_by_year()
+    if option == 2:
+        print('Do something')
+    if option == 3:
+        print('Do something')
+    if option == 4:
+        print('Do something')
+
+
+if __name__ == '__main__':
+    main()
