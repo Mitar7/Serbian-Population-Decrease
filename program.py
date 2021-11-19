4d4e3dc612896eaf1826e587b208ade1eb1bb4c2
Mitar7 <mitar.milovanovic100@gmail.com>
2021-11-19 12:21:03 +0100

    Added graphics

--- program.py
+++ program.py
@@ -7,7 +7,12 @@ msg = """
     Serbia's population decline problem
     -----------------------------------
     1. Population by selected year
+    2. Show population graphic (year <= 2021)
+    3. Show population graphic (year > 2020)
+    4. Show population graphic (1995 -> 2095)
+    5. Exit program
 """
+
 
 # print(df.loc['2030':, ['Year', 'TotalPopulation']])
 # print((df[df['TotalPopulation'] > 8000000]))
@@ -16,13 +21,43 @@ msg = """
 def filter_by_year():
     os.system('cls')
     year = int(input('Please enter a year: '))
-    if 0 < year < 3000: 
+    if 0 < year < 3000:
         if year < 2022:
             df = pd.read_csv('hist.csv')
             print(df.loc[df['Year'] == year, ['Year', 'TotalPopulation']])
         else:
             df = pd.read_csv('proj.csv')
             print(df.loc[df['Year'] == year, ['Year', 'TotalPopulation']])
+    x = input('Press any key to return to main menu...')
+    main()
+
+
+def graphic_rep_history():
+    df = pd.read_csv('hist.csv')
+    df['TotalPopulation'] = df['TotalPopulation'] / 1000000
+    df.plot(x='Year', y='TotalPopulation', kind='line', ylabel='Population in Millions', xlabel='Year')
+    plt.show()
+    x = input('Press any key to return to main menu...')
+    main()
+
+
+def graphic_rep_future():
+    df = pd.read_csv('proj.csv')
+    df['TotalPopulation'] = df['TotalPopulation'] / 1000000
+    df.plot(x='Year', y='TotalPopulation', kind='line', ylabel='Population in Millions', xlabel='Year')
+    plt.show()
+    x = input('Press any key to return to main menu...')
+    main()
+
+
+def graphic_rep_combine():
+    a = pd.read_csv('hist.csv')
+    b = pd.read_csv('proj.csv')
+    df = pd.merge(a, b, on=["Year", "TotalPopulation", "GrowthRate", "Density", "TotalPopulationRank", "DensityRank"],
+                  how='outer')
+    df['TotalPopulation'] = df['TotalPopulation'] / 1000000
+    df.plot(x='Year', y='TotalPopulation', kind='line', ylabel='Population in Millions', xlabel='Year')
+    plt.show()
     x = input('Press any key to return to main menu...')
     main()
 
@@ -34,11 +69,13 @@ def main():
     if option == 1:
         filter_by_year()
     if option == 2:
-        print('Do something')
+        graphic_rep_history()
     if option == 3:
-        print('Do something')
+        graphic_rep_future()
     if option == 4:
-        print('Do something')
+        graphic_rep_combine()
+    if option == 5:
+        exit()
 
 
 if __name__ == '__main__':
